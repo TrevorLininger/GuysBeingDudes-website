13d738a0ed77b4a09c988c8750ba28fc03f931ce
TrevorLininger <58058287+TrevorLininger@users.noreply.github.com>
2026-08-12 08:35:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const league = {
+    name: "Fantasy Football League",
+
+    managers: [
+        {
+            name: "Trevor",
+            championships: 4,
+            wins: 137,
+            playoffAppearances: 11
+        },
+        {
+            name: "Ryan",
+            championships: 2,
+            wins: 124,
+            playoffAppearances: 9
+        },
+        {
+            name: "Jake",
+            championships: 1,
+            wins: 119,
+            playoffAppearances: 8
+        }
+    ]
+};
+
+console.log(league);
