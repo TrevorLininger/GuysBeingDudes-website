e19168d45715b1d34ed18ec48a48e2b4c0961655
TrevorLininger <58058287+TrevorLininger@users.noreply.github.com>
2026-08-14 12:16:09 -0500

    Update managers.js

--- js/managers.js
+++ js/managers.js
@@ -3,5 +3,27 @@ console.log("MANAGERS JS LOADED");
 fetch("data/managers.json")
     .then(response => response.json())
     .then(managers => {
-        console.log(managers);
+
+        console.log("MANAGERS DATA:", managers);
+
+        const container = document.getElementById("managers-container");
+
+        managers.forEach(manager => {
+
+            const card = document.createElement("div");
+
+            card.className = "manager-page-card";
+
+            card.innerHTML = `
+                <div class="manager-page-card-info">
+                    <h2>${manager.managerName}</h2>
+                    <p>${manager.teamName}</p>
+                </div>
+            `;
+
+            container.appendChild(card);
+        });
+    })
+    .catch(error => {
+        console.error("ERROR LOADING MANAGERS:", error);
     });
